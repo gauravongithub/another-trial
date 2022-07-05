9595e1db1445251828912d5e5a64e55e42f03e71
Gaurav <gaurav.tiwari3001@gmail.com>
2022-07-05 17:14:42 +0530

    Added heart to the projects

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -9,10 +9,10 @@ const ProductList = () => {
   return (
     <div className="p1">
       <div className="p1-texts">
-        <h1 className="p1-title">These are some of my works</h1>
+        <h1 className="p1-title">❤These are some of my works❤</h1>
         <p className="p1-desc">
           Hello, these are my projects which I have made while learning Web Development. As stated I have made these projects using MERN tech stack(MongoDB, ExpressJs, ReactJs, NodeJs).
-          --To Visit these projects, please click on the image.
+          To Visit these projects, please click on the image.
         </p>
       </div>
       <div className="p1-list">
